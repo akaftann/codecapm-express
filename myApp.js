94671694c309dc72cc337aaab31cd27d83b4267f
mac <mac@MacBook-Pro-mac.local>
2023-04-21 22:53:15 +0300

    dotenv

--- myApp.js
+++ myApp.js
@@ -1,12 +1,17 @@
 let express = require('express');
 let app = express();
+let dotenv = require('dotenv').config();
 app.use("/public",express.static(__dirname + "/public"))
 app.get('/',function(req,res){
     const absolutePath = __dirname + '/views/index.html'
     res.sendFile(absolutePath);
 });
 app.get('/json',(req,res)=>{
-    res.json({"message":"Hello json"})
+    if(process.env.MESSAGE_STYLE === 'uppercase'){
+        res.json({"message":"HELLO JSON"})
+    }else{
+        res.json({"message":"Hello json"})
+    }
 })
 
 
