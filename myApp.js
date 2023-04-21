0f5a237ec26e6434fb8fe472c7087ff6a06a03c0
mac <mac@MacBook-Pro-mac.local>
2023-04-21 21:33:14 +0300

    4

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+app.use(express.static('public'))
 app.get('/',function(req,res){
     const absolutePath = __dirname + '/views/index.html'
     res.sendFile(absolutePath);
