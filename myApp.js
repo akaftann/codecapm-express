c28f265f3abfe8f5f885f4d24d08e67787a89661
mac <mac@MacBook-Pro-mac.local>
2023-04-21 21:16:30 +0300

    2

--- myApp.js
+++ myApp.js
@@ -1,9 +1,8 @@
 let express = require('express');
 let app = express();
 app.get('/',function(req,res){
-    res.send('Response String');
+    res.send('Hello Express');
 });
-console.log("Hello Express");
 
 
 
