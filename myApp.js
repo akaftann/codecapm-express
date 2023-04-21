62d456c165bc91c759ee3020d9673c2fe5cd83af
mac <mac@MacBook-Pro-mac.local>
2023-04-21 23:10:58 +0300

    middleware

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require('express');
 let app = express();
 let dotenv = require('dotenv').config();
+app.use((req,res,next)=>{
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+})
 app.use("/public",express.static(__dirname + "/public"))
 app.get('/',function(req,res){
     const absolutePath = __dirname + '/views/index.html'
