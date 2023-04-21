fbeb22ff43e202ab77f823ba04b0b32075a43416
mac <mac@MacBook-Pro-mac.local>
2023-04-21 20:49:28 +0300

    added res string

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
 let express = require('express');
 let app = express();
-console.log("Hello Express")
 app.get('/',function(req,res){
     res.send('Response String');
 })
+console.log("Hello Express");
 
 
 
